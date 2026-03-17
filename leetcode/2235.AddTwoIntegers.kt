51710d190b439a19af49d91a2ed8a52ad55570e3
Luiza Rehbein <l254303@dac.unicamp.br>
2026-03-17 18:29:01 -0300

    Adding LeetCode 2235

---
+++ leetcode/2235.AddTwoIntegers.kt
@@ -0,0 +1,16 @@
+/* 
+ * Problem:
+ * Given two integers num1 and num2, return the sum of the two integers.
+ *
+ * Approach:
+ * Return the sum of the two given integers.
+ * 
+ * Time Complexity: O(1)
+ * Space Complexity: O(1)
+ */
+
+ class Solution {
+    fun sum(num1: Int, num2: Int): Int {
+        return num1 + num2
+    }
+}
